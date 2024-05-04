5a4f6c532f6a43c80cdca638e3a353f11e3c299f
mastefelix <53604895+mastefelix@users.noreply.github.com>
2024-05-04 17:02:48 +0300

    Update send.php

--- send.php
+++ send.php
@@ -7,12 +7,11 @@ $name = trim(urldecode(htmlspecialchars($name)));
 $email = trim(urldecode(htmlspecialchars($email)));
 $msg = trim(urldecode(htmlspecialchars($msg)));
 
-if(mail("alexandr.kurganskii!yandex.ru",
+if(mail("alexandr.kurganskii@yandex.ru",
     "Новый запрос",
     "<h1>На сайте оставлена заявка</h1>
     <br>от:<b>".$name."</b>
     <br>email:<b>".$email."</b>
-    <br>от:<b>".$name."</b>
     <br>комментарий:".$msg."
     <br>Поздравляю с новой заявкой",
     "From: 1c-webdevelopment.ru\r\n". "Content-type: text/html\r\n"))
